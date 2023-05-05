112812f9204f18858023b3415c519dfcab691c58
ayakuma309 <ayako.30901@gmail.com>
2023-05-05 17:40:38 +0900

    ✨ add:ログイン時にuser情報がdbに登録するようにした

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import uuid from 'react-uuid';
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
 import {
   User,
@@ -8,6 +9,16 @@ import {
   onAuthStateChanged,
   GoogleAuthProvider,
 } from "firebase/auth";
+
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  query,
+  where,
+} from 'firebase/firestore'
 
 type UserState = User | null; // null の場合はログインしていない状態
 const userState = atom<UserState>({
@@ -40,10 +51,22 @@ export const useAuth = (): boolean => {
 
   useEffect(() => {
     const auth = getAuth();
-		//4.ユーザ認証を監視し,変更があったときに引数のコールバック関数を実行
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       setUser(user);
       setIsLoading(false);
+      //DB保存する
+      if (user) {
+        const db = getFirestore();
+        const userRef = doc(db, "users", user.uid); // ユーザー情報を取得
+        const userDoc = await getDoc(userRef);
+        if (!userDoc.exists()) {
+          await setDoc(userRef, {
+            id: uuid(),
+            displayName: user.displayName,
+            email: user.email,
+          });
+        }
+      }
     });
     return unsubscribe;
   }, [setUser]);
