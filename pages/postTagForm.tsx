8fbdc14ea43ecd4be86e0e80bc955814137d6794
ayakuma309 <ayako.30901@gmail.com>
2023-05-06 13:56:06 +0900

    ✨ add:投稿画面で登録したtagを表示

--- pages/postTagForm.tsx
+++ pages/postTagForm.tsx
@@ -1,16 +1,22 @@
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
 import { NextPage } from 'next';
-import { doc, getFirestore, collection, addDoc } from "firebase/firestore";
+import { doc, getFirestore, collection, addDoc, query, onSnapshot } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import Link from "next/link";
 import MyPageButton from "@/components/atoms/MyPageButton";
 
+interface Tags {
+  text: string;
+}
 const postTagForm: NextPage = () => {
   const auth = getAuth();
   const currentUser = auth.currentUser;
   const db = getFirestore();
   const [tag, setTag] = useState<string>("");
-
+  const [tags, setTags] = useState<Tags[]>([
+    {
+      text: ""
+    }
+  ])
   //tagをfirestoreに保存
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,6 +31,25 @@ const postTagForm: NextPage = () => {
       console.log(error);
     }
   };
+
+  //tagをfirestoreから取得
+  useEffect(() => {
+    if(currentUser){
+      const db = getFirestore();
+      const ref = collection(doc(db, "users", currentUser.uid), "tags");
+      const q = query(ref);
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const gotTags: Tags[] = [];
+        querySnapshot.forEach((doc) => {
+          gotTags.push({
+            text: doc.data().text,
+          });
+        });
+        setTags(gotTags);
+      });
+      return unsubscribe;
+    }
+  },[currentUser, tag]);
 
   return (
     <>
@@ -51,6 +76,13 @@ const postTagForm: NextPage = () => {
             </button>
           </div>
         </form>
+        <div>
+          {tags.map((tag) =>(
+            <div className="text-white text-lg">
+              {tag.text}
+            </div>
+          ))}
+        </div>
         {/*戻るボタンの追加 */}
         <MyPageButton>戻る</MyPageButton>
       </div>
