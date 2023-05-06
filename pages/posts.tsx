ac9d7e9a6eb538a834151f3afe9833178196b2d7
ayakuma309 <ayako.30901@gmail.com>
2023-05-06 10:49:41 +0900

    ✨ add: tagの表示

---
+++ pages/posts.tsx
@@ -0,0 +1,54 @@
+import { NextPage } from 'next'
+import React from 'react'
+import { TagCloud } from 'react-tagcloud'
+import styles from "@/styles/post.module.css"
+const data = [
+  { value: 'jQuery', count: 25 },
+  { value: 'MongoDB', count: 18 },
+  { value: 'JavaScript', count: 38 },
+  { value: 'React', count: 30 },
+  { value: 'Nodejs', count: 28 },
+  { value: 'Express.js', count: 25 },
+  { value: 'HTML5', count: 33 },
+  { value: 'CSS3', count: 20 },
+  { value: 'Webpack', count: 22 },
+  { value: 'Babel.js', count: 7 },
+  { value: 'ECMAScript', count: 25 },
+  { value: 'Jest', count: 15 },
+  { value: 'Mocha', count: 17 },
+  { value: 'React Native', count: 27 },
+  { value: 'Angular.js', count: 30 },
+  { value: 'TypeScript', count: 15 },
+  { value: 'Flow', count: 30 },
+  { value: 'NPM', count: 11 },
+]
+
+const customRenderer = (tag: any, size: any, color: any) => (
+  <span
+    key={tag.value}
+    style={{
+      animation: 'blinker 3s linear infinite',
+      animationDelay: `${Math.random() * 2}s`,
+      fontSize: `${size / 2}em`,
+      border: `2px solid ${color}`,
+      margin: '3px',
+      padding: '3px',
+      display: 'inline-block',
+      color: 'white',
+    }}
+  >
+    {tag.value}
+  </span>
+)
+const posts: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <TagCloud
+        tags={data} minSize={1} maxSize={5} renderer={customRenderer}
+        onClick={tag => alert(`'${tag.value}' was selected!`)}
+      />
+    </div>
+  )
+}
+
+export default posts;
