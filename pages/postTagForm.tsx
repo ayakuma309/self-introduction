dcfe15f40f2b59858313b2c374e6312d92da9e74
ayakuma309 <ayako.30901@gmail.com>
2023-05-06 12:55:19 +0900

    🎨 Tagの配置場所変更(Userの詳細画面にいくようにした)

---
+++ pages/postTagForm.tsx
@@ -0,0 +1,67 @@
+import {  useState } from "react";
+import { NextPage } from 'next';
+import { doc, getFirestore, collection, addDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import Link from "next/link";
+
+const postTagForm: NextPage = () => {
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
+  const db = getFirestore();
+  const [tag, setTag] = useState<string>("");
+
+  //tagをfirestoreに保存
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try{
+      if(currentUser){
+        await addDoc(collection(doc(db, "users", currentUser.uid), "tags"), {
+          text: tag,
+        });
+        setTag("");
+      }
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full max-w-xs mx-auto my-10">
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label htmlFor="tags" className="block text-gray-700 font-bold mb-2">
+              好きなものを保存
+            </label>
+            <input
+              name="tags"
+              type="text"
+              value={tag}
+              onChange={(e) => setTag(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        {/*戻るボタンの追加 */}
+        <Link href={`/users/${currentUser?.uid}`}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Back
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+
+export default postTagForm;
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -4,6 +4,7 @@ import { User } from "../../types/User";
 import {collection, getDoc, getFirestore, doc} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import Link from "next/link";
+import Tags from "@/components/Tags";
 
 type Query = {
   id: string;
@@ -39,7 +40,7 @@ export default function UserPage() {
     //ログインしているユーザーがユーザー情報のuidと一致している場合
     if (currentUser && currentUser.uid == query.id) {
       return(
-        <Link href="/postForm">
+        <Link href="/postTagForm">
           <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
@@ -54,16 +55,19 @@ export default function UserPage() {
   return (
     <div>
       {user ? (
-        <div className="w-90 mx-auto text-center my-10">
-          {/* user が存在する場合のみに囲んだ部分の描画が行われる */}
-          {user && (
-            <div >
-              <h1 className="h4">{user.displayName}さん</h1>
-              <div className="m-5">{user.id}</div>
-              {handleButtonClick()}
-            </div>
-          )}
-        </div>
+        <>
+          <div className="w-90 mx-auto text-center my-10">
+            {/* user が存在する場合のみに囲んだ部分の描画が行われる */}
+            {user && (
+              <div >
+                <h1 className="h4">{user.displayName}さん</h1>
+                <div className="m-5">{user.id}</div>
+                {handleButtonClick()}
+              </div>
+            )}
+          </div>
+          <Tags/>
+        </>
       ) : (
         <p>ロード中…</p>
       )}
