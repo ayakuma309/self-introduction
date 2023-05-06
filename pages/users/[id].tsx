752948035fe7bb8a98b7888cd638db828557a504
ayakuma309 <ayako.30901@gmail.com>
2023-05-06 17:09:45 +0900

    top戻るボタンの追加

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -49,7 +49,15 @@ export default function UserPage() {
         </Link>
       )
     }else{
-      return null;
+      return (
+        <Link href="/">
+          <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+            TOP
+          </button>
+        </Link>
+      );
     }
   }
   return (
